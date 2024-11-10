c1eb36367a85504b0b416c4a8b59bb53efd0f971
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-10 14:17:53 +0530

    While loops

--- Looping_Statements/while.py
+++ Looping_Statements/while.py
@@ -15,9 +15,18 @@
 
 #Continue statement using while loop:
 
+# i=1
+# while i<10:
+#     i=i+1
+#     if i==5:
+#         continue
+#     print(i)
+
+#Else in while 
 i=1
 while i<10:
-    i=i+1
-    if i==5:
-        continue
     print(i)
+    i=i+1
+
+else:
+    print("i is no longer less than 10")
