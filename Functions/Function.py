4dc42f1ca06bdec871897d6b911ed7524e207b26
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-10 15:18:05 +0530

    Function in python

---
+++ Functions/Function.py
@@ -0,0 +1,12 @@
+
+# A function is a block of code which only runs when it is called.
+
+# You can pass data, known as parameters, into a function.
+
+# A function can return data as a result.
+
+#Create Function:
+def my_func():  #function createion
+    print("This is function")
+
+my_func()   #Calling dunction
