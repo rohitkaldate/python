f187ac2dd091a42e4850b24db15ee870a74a2e1f
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-10 14:28:38 +0530

    For loops

--- Looping_Statements/forloop.py
+++ Looping_Statements/forloop.py
@@ -1,3 +1,14 @@
 fruits=["apple","banana","kiwi","cherry"]
-for x in fruits:
-    print(x)
+# for x in fruits:
+#     print(x)
+
+# for i in 'apple':
+#     print(i)
+
+
+#Break statement in for loop:
+for i in fruits:
+    print(i)
+
+    if i=="kiwi":
+        break
