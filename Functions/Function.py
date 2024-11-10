6542cacf185b36506023980adef5872d19c640ab
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-10 15:27:37 +0530

    Function in python

--- Functions/Function.py
+++ Functions/Function.py
@@ -6,16 +6,23 @@
 # A function can return data as a result.
 
 #Create Function:
-# def my_func():  #function createion
+# def my_func():  #function creation
 #     print("This is function")
 
 # my_func()   #Calling function
 
 #Passing arguments in function:
+# def name(fname):    #Creating function with the single argument
+#     print(fname+"kaldate")
 
-def name(fname):
-    print(fname+"kaldate")
+# name("Rohit") #Calling function with single argument
+# name("Amit") #Calling function with single argument
+# name("Ajit") #Calling function with single argument
 
-name("Rohit")
-name("Amit")
-name("Ajit")
+##Calling function using 2 arguments:
+
+def data(fname,lname):
+    print(fname + " " +lname)
+data("Rohit","Kaldate")
+data("Amit","Kaldate")
+data("Ajit","Kaldate")
