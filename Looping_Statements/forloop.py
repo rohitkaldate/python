f5df685146c7c498ca75ec9ff3e8cd03734b96c4
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-10 15:05:15 +0530

    For loops

--- Looping_Statements/forloop.py
+++ Looping_Statements/forloop.py
@@ -14,7 +14,25 @@ fruits=["apple","banana","kiwi","cherry"]
 #         break
 
 #print after break:
-for x in fruits:
-    if x=="cherry":
-        break
-    print(x)
+# for x in fruits:
+#     if x=="cherry":
+#         break
+#     print(x)
+
+
+#conitunue statement using for loop:
+# for c in fruits:
+#     if c == "kiwi":
+#         continue
+#     print(c)
+
+#Range function in for loop:
+#This range( ) function starts taking the values from the 0.
+
+# for i in range(10):
+#     print(i)
+
+#range with starting value and also used if condition to print even no's:
+for i in range(2,10):
+    if(i%2==0):
+        print("Even no's from 2 to 10 are: ",i)
