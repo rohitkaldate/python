79af0eb605201376657d23004e8db02bc7d9fc99
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-10 13:06:02 +0530

    Looping statements

--- Looping_Statements/if.py
+++ Looping_Statements/if.py
@@ -1,6 +1,14 @@
-x=("banana","kiwi","cherry")
-print(x)
-print("type of x is:",type(x))
+# x=("banana","kiwi","cherry")
+# print(x)
+# print("type of x is:",type(x))
 
-if "cherry" in x:
-    print("Cherry is present in tuple")
+# if "cherry" in x:
+#     print("Cherry is present in tuple")
+
+
+a=33
+b=33
+if(a>b):
+    print("A is lareger")
+elif a==b:
+    print("Both are equal")
