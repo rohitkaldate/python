0472693a7a464ad7be62a43b043c84cecaa6f847
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-10 13:22:33 +0530

    While loops

--- Looping_Statements/while.py
+++ Looping_Statements/while.py
@@ -1,6 +1,14 @@
+##Table 
+# i=1
+# while i<=10:
+#     print(i*2)
+#     i=i+1
+
+
+# using bresk statement in while loop:
 i=1
-while i<=10:
-    print(i*2)
+while i<20:
+    print(i)
+    if i==12:
+        break
     i=i+1
-
-
