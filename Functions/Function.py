02cd44f7a7d1994b0b1d53a47b473e481301387b
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-10 15:45:51 +0530

    Function in python

--- Functions/Function.py
+++ Functions/Function.py
@@ -31,8 +31,27 @@
 #     print("Youngest brother is: "+kids[2])
 # children("Rohit","amit","Ajit")
 
-#Keyword arguments : Sending arguments with the key
 
-def kids(child2,child1,child3):
-    print("Youngest child is:"+ child3)
-kids(child1="Rohit",child3="amit",child2="Ajit")
+#Keyword arguments : Sending arguments with the key
+# def kids(child2,child1,child3):
+#     print("Youngest child is:"+ child3)
+# kids(child1="Rohit",child3="amit",child2="Ajit")
+
+
+##Default parameter value:
+# def my_func(country="India"):
+#     print("I am from ",""+country)
+
+# my_func()
+# my_func("USA")
+# my_func("UK")
+
+
+## Passing a list as an argument:
+
+def my_function(food):
+    for x in food:
+        print(x)
+    
+fruits=["Apple","Mango","Kiwi","Cherry"]
+my_function(fruits)
