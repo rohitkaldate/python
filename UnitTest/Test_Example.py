b4c080c02001ef18e4dd2cd30b402a3149281f1d
Rohit Kaldate <kaldaterohit26@gmail.com>
2024-11-17 20:03:45 +0530

    Test Cases

--- UnitTest/Test_Example.py
+++ UnitTest/Test_Example.py
@@ -1,20 +1,19 @@
 import unittest
 
-def add(a,b):
-    return a+b
+from Add_numbers import add
 
-class AddNumbers(unittest.TestCase):
+class Add_Numbers(unittest.TestCase):
     def test_add_positive(self):
         result = add(2,3)
-        self.assertEqual(result,67)
+        self.assertEqual(result,8)
         
-    def test_add_negative(self):
-        result= add(-2,-3)
-        self.assertEqual(result,-5)
+    # def test_add_negative(self):
+    #     result= Add_numbers.add(-2,-3)
+    #     self.assertEqual(result,-5)
         
-    def test_add_mixed(self):
-        result= add(3,-5)
-        self.assertEqual(result,-2)
+    # def test_add_mixed(self):
+    #     result= Add_numbers.add(3,-5)
+    #     self.assertEqual(result,-2)
         
 if __name__=='__main__':
     unittest.main()
