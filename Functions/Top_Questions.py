9003d98e29ffb5e682ae389a8f2838a30a2eed17
rohitkaldate <rkaldate@cisco.com>
2025-04-06 15:47:20 +0530

    [RAND-1]: Factorial of number
    https://jira-eng-rtp3.cisco.com/jira/browse/RAND-1

---
+++ Functions/Top_Questions.py
@@ -0,0 +1,22 @@
+##WAP to print the factorial of the number using the return :::
+# n=int(input("Enter no : "))
+# def fact(n):
+#     result=1
+#     while n>=1:
+#         result=result*n
+#         n=n-1
+#     return result
+# f=fact(n)
+# print(f)
+
+
+##WAP to print the factorial of the number using the recursive function:::
+# n = int(input("Enter no : "))
+
+# def fact(n):
+#     if n == 0:
+#         res=1
+#     else:
+#         res=n*fact(n-1)
+#     return res
+# print(fact(n))
